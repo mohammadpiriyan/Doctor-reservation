feed7d3bc20a522f81531b8effb6dd3d6d367a10
msdmhmdi <masoudmhmdi86@gmail.com>
2023-03-16 16:39:47 +0330

    convert innerHTML to creteElement in renderUI function

--- my-project/src/components/btnCard.js
+++ my-project/src/components/btnCard.js
@@ -1,3 +1,1 @@
-export const btnCard = () => {
-  console.log("slm mmd");
-};
+export const btnCard = () => {};
--- my-project/src/components/getDataDocUi.js
+++ my-project/src/components/getDataDocUi.js
@@ -1,7 +1,7 @@
-import { renderUi } from "./renderUi";
+import { renderUi } from './renderUi';
 
 export const getDataDocUi = () => {
-  fetch("http://localhost:3002/doctors")
+  fetch('http://localhost:3002/doctors')
     .then((response) => response.json())
     .then((data) => renderUi(data));
 };
